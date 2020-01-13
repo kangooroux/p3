7e09d5166b951bcfdb256a6ae88081fbdd63a600
kangooroux <bousquetalexandrepro@gmail.com>
2020-01-13 20:17:42 +0100

    début du travail sur le frontend

---
+++ view/frontend/header.php
@@ -0,0 +1,6 @@
+<header>
+  <div class="logo_gbaf">
+    <img src="\p3\public\images\15631755744257_LOGO_GBAF_ROUGE.png" alt="">
+  </div>
+  <!-- Si la personne est connectée alors son nom-prénom apparaîtront ainsi que les boutons déconnecter et paramètres du compte-->
+</header>
---
+++ view/frontend/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="/p3/public/css/style.css">
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
